c081ee3a2caf17e40fe1f13fd2bd8efd4bc18e2d
rend1027 <rend1027@yahoo.com>
2024-08-01 17:24:20 -0400

    Created navbar with layout

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navbar from './components/navbar/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <Navbar/>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import flo from "../../images/flo.png";
+import github from '../../images/github.png'
+import linkedin from "../../images/linkedin.png";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <a href="#home" className={styles.flo}>
+        <img src={flo} alt="Logo" />
+      </a>
+      <div className={styles.navLinks}>
+        <ul className={styles["navbar-nav"]}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contacts">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <a href="#github" className={styles.github}>
+        <img src={github} alt='Github'/>
+      </a>
+      <a href="#linkedin" className={styles.linkedin}>
+        <img src={linkedin} alt="Linkedin" />
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
