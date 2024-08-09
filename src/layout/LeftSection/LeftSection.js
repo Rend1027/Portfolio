005d82c58b1ef15e0ff8c46e2e5f3afb79fd0b6f
rend1027 <rend1027@yahoo.com>
2024-08-09 16:31:40 -0400

    Added CSS to HomePage.

--- src/layout/LeftSection/LeftSection.js
+++ src/layout/LeftSection/LeftSection.js
@@ -1,12 +1,12 @@
 import React from "react";
-import thisOne from "../../images/thisOne.jpg";
+import coverLetterPic from "../../images/coverLetterPic.jpg";
 import styles from "./LeftSection.module.css";
 
 const LeftSection = () => {
   return (
     <div className={styles.leftSection}>
       <h1 className={styles.header}>Fullstack Engineer</h1>
-      <img src={thisOne} alt="" className={styles.image} />
+      <img src={coverLetterPic} alt="" className={styles.image} />
     </div>
   );
 };
