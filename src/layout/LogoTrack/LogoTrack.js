684e2206db3e736f552cc5917958bd4e37fa5de9
rend1027 <rend1027@yahoo.com>
2024-08-10 16:02:19 -0400

    Added css to logotrack and it looks epic.

--- src/layout/LogoTrack/LogoTrack.js
+++ src/layout/LogoTrack/LogoTrack.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import azure from '../../assets/images/logos/azure.png'
-import css from '../../assets/images/logos/css.png'
-import html from '../../assets/images/logos/html.png'
-import javascript from '../../assets/images/logos/javascript.png'
-import nextjs from '../../assets/images/logos/nextjs.png'
-import vscode from '../../assets/images/logos/vscode.png'
-
+import React from "react";
+import azure from "../../assets/images/logos/azure.png";
+import css from "../../assets/images/logos/css.png";
+import html from "../../assets/images/logos/html.png";
+import javascript from "../../assets/images/logos/javascript.png";
+import nextjs from "../../assets/images/logos/nextjs.png";
+import vscode from "../../assets/images/logos/vscode.png";
+import react from "../../assets/images/logos/react.png";
+import styles from "./LogoTrack.module.css";
 
 const LogoTrack = () => {
-    return (
-        <div className='container'>
-            <img src={azure} alt='Azure'/>
-            <img src={css} alt='CSS'/>
-            <img src={html} alt='HTML'/>
-            <img src={javascript} alt='Javascript'/>
-            <img src={nextjs} alt='Nextjs'/>
-            <img src={vscode} alt='VScode'/>
+  return (
+    <div className={`${styles.logos} ${styles.container}`}>
+      <div className={styles.marquee}>
+        <div className={styles.track}>
+          <img src={azure} alt="Azure" />
+          <img src={css} alt="CSS" />
+          <img src={html} alt="HTML" />
+          <img src={javascript} alt="Javascript" />
+          <img src={nextjs} alt="Nextjs" />
+          <img src={vscode} alt="VScode" />
+          <img src={react} alt="React" />
+          <img src={azure} alt="Azure" />
+          <img src={css} alt="CSS" />
+          <img src={html} alt="HTML" />
+          <img src={javascript} alt="Javascript" />
+          <img src={nextjs} alt="Nextjs" />
+          <img src={vscode} alt="VScode" />
+          <img src={react} alt="React" />
+          <img src={azure} alt="Azure" />
+          <img src={css} alt="CSS" />
+          <img src={html} alt="HTML" />
+          <img src={javascript} alt="Javascript" />
+          <img src={nextjs} alt="Nextjs" />
+          <img src={vscode} alt="VScode" />
+          <img src={react} alt="React" />
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LogoTrack;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,19 @@ import React from "react";
 import LeftSection from "../../layout/LeftSection/LeftSection";
 import ProjectSlider from "../../layout/RightSection/ProjectSlider/ProjectSlider";
 import LogoTrack from "../../layout/LogoTrack/LogoTrack";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
-      <LeftSection />
-      <ProjectSlider />
-      <LogoTrack/>
-    </>
+    <div className={styles.homeContainer}>
+      <div className={styles.mainContent}>
+        <LeftSection />
+        <ProjectSlider />
+      </div>
+      <div >
+        <LogoTrack />
+      </div>
+    </div>
   );
 };
 
